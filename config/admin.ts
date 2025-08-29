2b594a6944320eb883bf59554dc07dccd1a80116
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-08-29 15:24:20 +0200

    strapi admin panel

--- config/admin.ts
+++ config/admin.ts
@@ -1,3 +1,4 @@
+/*
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
@@ -16,5 +17,29 @@ export default ({ env }) => ({
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+});
+*/
+
+export default ({ env }) => ({
+  url: env("ADMIN_PATH", "/strapi/admin"),
+  serveAdminPanel: true,
+  auth: {
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT"),
+    },
+  },
+  secrets: {
+    encryptionKey: env("ENCRYPTION_KEY"),
+  },
+  flags: {
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });

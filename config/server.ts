0d2d551bbf7fcc87fd841faefbf3c9cf1d7decbc
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-08-29 12:50:16 +0200

    path test

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,20 @@
+/*
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+});
+*/
+
+export default ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    url: env("ADMIN_PATH", "/admin"),
   },
 });

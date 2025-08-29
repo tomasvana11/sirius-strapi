676a67c972c4f7df8ff7429f5b4a0dba53b38578
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-08-29 17:59:47 +0200

    fix

--- config/admin.ts
+++ config/admin.ts
@@ -1,29 +1,4 @@
-/*
 export default ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
-});
-*/
-
-export default ({ env }) => ({
-  url: "/admin",
-  serveAdminPanel: true,
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
   },
--- config/server.ts
+++ config/server.ts
@@ -1,34 +1,7 @@
-/*
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
-  },
-});
-*/
-
-/*
-export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  admin: {
-    url: env("ADMIN_PATH", "/strapi/admin"),
-  },
-});
-*/
-
-export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  url: env("PUBLIC_URL", "http://localhost:1337/strapi"),
-  dirs: {
-    public: "./public",
   },
 });

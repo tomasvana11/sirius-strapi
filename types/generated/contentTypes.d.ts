8e3acce5c33365b1627a90979f64eec6deff6e10
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-10-06 17:10:58 +0200

    repeatable comp fix

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -387,7 +387,7 @@ export interface ApiAboutAbout extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    HeroBanner: Schema.Attribute.Component<'components.hero-banner', true>;
+    HeroBanner: Schema.Attribute.Component<'components.hero-banner', false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::about.about'> &
       Schema.Attribute.Private;

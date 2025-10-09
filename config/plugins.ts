7e5823d804fd53a06aa202c82773a4baef0666fc
tomas-avette <156674165+tomas-avette@users.noreply.github.com>
2025-10-09 13:51:29 +0200

    DO spaces

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,28 @@
-export default () => ({});
+export default () => ({
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          baseUrl: process.env.DO_SPACE_CDN_URL,
+          rootPath: process.env.DO_SPACE_DIRECTORY,
+          s3Options: {
+            credentials: {
+              accessKeyId: process.env.DO_SPACE_ACCESS_KEY,
+              secretAccessKey: process.env.DO_SPACE_SECRET_KEY,
+            },
+            endpoint: process.env.DO_SPACE_ENDPOINT,
+            region: process.env.DO_SPACE_REGION,
+            params: {
+              Bucket: process.env.DO_SPACE_BUCKET,
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
+
